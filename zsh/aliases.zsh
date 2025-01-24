c1e96c42b8ea6657adc8aee187b816443d077b5c
hstoklosa <hubert.stoklosa23@gmail.com>
2025-01-24 17:19:39 +0000

    add shortcut and directory aliases to zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,11 @@
 # Shortcuts
 alias reload=". ~/.zshrc"
 alias cls="clear"
+alias ls="ls --color=none"
+alias lal="ls -al"
+alias pps="ps -e -o user,state,pid,ppid,command"
+
+# Directories
+alias ~p="cd $HOME/projects"
+alias ~w="cd $WIN"
+alias ~m="cd $MEGA"
