dffdf6cba17ded7fe9422e68a64cb7da0d894cb2
hstoklosa <hubert.stoklosa23@gmail.com>
2025-02-10 02:13:34 +0000

    update binaries within brewer

--- macos/brew.sh
+++ macos/brew.sh
@@ -1,2 +1,22 @@
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install more recent versions of some macOS tools.
+brew install vim --with-override-system-vi
+brew install grep
+brew install openssh
+
+# Install OS-related tools.
 brew install --cask alt-tab
 brew install --cask rectangle
+
+# Install other useful binaries.
+brew install git
+brew install git-lfs
+brew install btop
+
+# Remove outdated versions from the cellar.
+brew cleanup
